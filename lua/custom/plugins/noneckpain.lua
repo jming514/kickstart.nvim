976169f08c4da5f3a9e079591b3c8627106fdd39
jming514 <tang_lester@hotmail.com>
2023-10-01 22:20:07 -0400

    disable no neck pain

--- lua/custom/plugins/noneckpain.lua
+++ lua/custom/plugins/noneckpain.lua
@@ -1,15 +1,15 @@
 return {
-	"shortcuts/no-neck-pain.nvim",
-	config = function()
-		require('no-neck-pain').setup {
-			autocmds = {
-				enableOnVimEnter = true
-			},
-			integrations = {
-				Neotree = {
-					reopen = false
-				}
-			}
-		}
-	end
+	-- 	"shortcuts/no-neck-pain.nvim",
+	-- 	config = function()
+	-- 		require('no-neck-pain').setup {
+	-- 			autocmds = {
+	-- 				enableOnVimEnter = true
+	-- 			},
+	-- 			integrations = {
+	-- 				Neotree = {
+	-- 					reopen = false
+	-- 				}
+	-- 			}
+	-- 		}
+	-- 	end
 }
